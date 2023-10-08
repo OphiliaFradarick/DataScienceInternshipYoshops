71feb6a2803aa36fd7449d73af31763f4f1d7c9c
Ophilia Fradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2023-10-08 09:20:04 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,13 @@
+#list 
+planets = ["earth", "mars", "jupiter"]
+name = "Leo the 13th"
+
+# for i in planets:
+#     print(i)
+
+# range(start, end+1, step)
+
+# for num in range(70,49,-5):
+#   print(num)
+
+print(planets[-2])
